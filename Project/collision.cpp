0eeb06219b37472557bace1fd646a7bc04902885
ireven <hensati11@gmail.com>
2025-07-19 16:24:31 +0900

    プロジェクトを更新

---
+++ Project/collision.cpp
@@ -0,0 +1,14 @@
+#include"DxLib.h"
+#include"collision.h"
+#include"player.h"
+#include"enemy.h"
+
+void hitPlayerWithEnemy(Enemy* enemy,Player* player)
+{
+
+}
+
+void hitShotWithEnemy(Enemy* enemy, Player* player)
+{
+
+}
---
+++ Project/collision.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class Enemy;
+class Player;
+
+void hitPlayerWithEnemy(Enemy* enemy, Player* player);
+void hitShotWithEnemy(Enemy* enemy, Player* player);
